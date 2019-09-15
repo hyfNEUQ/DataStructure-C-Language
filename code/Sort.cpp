9b4e3d1d00d11cba0dd321a69d25d2ac010866e9
hyfNEUQ <1762263688@qq.com>
2019-09-15 18:02:14 +0800

    重写了堆排序

--- code/Sort.cpp
+++ code/Sort.cpp
@@ -147,7 +147,6 @@ print(data,n);
 //快速排序
 void quickSort(int data[],int start,int end)
 {
-	cout<<"--------------------快速排序------------------------------"<<endl;
 	if(!(end>start)) return;
 	int i=start;
 	int j=end;
@@ -166,47 +165,65 @@ void quickSort(int data[],int start,int end)
 			i++;
 		}
 		swap(data,i,j);
-		print(data,15);	
 	}
+	cout<<"--------------------快速排序------------------------------"<<endl;
+	print(data,15);	
 	quickSort(data,start,i-1);
 	quickSort(data,i+1,end);	 
 } 
-//堆排序 (构造极大堆)
-void  buildMinHeap(int data[],int n)
+//(向下调整成极小堆 param 数组,当前结点的下标,数组元素个数 
+void  adjustDown(int data[],int index,int n)
 {
-	if(n==0) return;
-	for(int i=n;i>=0;i--)
-	{
-		int x=2*i+1;int y=2*i+2;
-		if(x>n) continue;
-		else if(x==n)
+	for(int i=index;i<n;) 
+	{ 
+		int low=2*i+1;
+		int high=2*i+2;
+		if(low>=n) return ;
+		if(high>=n)
 		{
-			if(data[x]>data[i]) 
+		if(data[low]<data[index]) swap(data,index,low);	
+		return;
+		} 
+		else
+		{
+			int j=low;
+			if(data[high]<data[low]) j=high;
+			//j 为子节点中较小的结点
+			if(data[j]<data[i]) 
 			{
-				swap(data,x,i);
+				swap(data,i,j);
+				i=j; 
 			}
+			else
+			{
+			return ;	
+			} 
 		}
-		else 
-		{
-			    int index=x;
-			    if(data[y]>data[x]) index=y;
-			    if(data[index]<data[i]) continue; 
-		        swap(data,index,i);
-		}
-	}	
+	}
 } 
-//堆排序 
+//建立小根堆 
+void builHeap(int data[],int n)
+{
+	for(int i=n/2+1;i>=0;i--)
+	{
+		adjustDown(data,i,n);
+	}
+}
+//堆排序
 void heapSort(int data[],int n)
 {
-	cout<<"--------------------堆排序------------------------------"<<endl;
-	while(n>=0)
+	//建立极小堆 
+	for(int i=n/2+1;i>=0;i--)
 	{
-		buildMinHeap(data,n);
-		swap(data,n,0);
-		n--;
-	}	
-	print(data,17);
-}
+		adjustDown(data,i,n);
+	}
+	for(int i=n-1;i>0;i--)
+	{
+		swap(data,i,0);
+		adjustDown(data,0,i-1);
+	}
+	print(data,n);
+} 
 //合并两数组 
 void merge(int start,int mid,int end,int data[],int tempt[])
 {
@@ -358,9 +375,147 @@ while(!stack.empty())
 	print(data,17);	
 }
 } 
+//把所有奇数移到所有偶数前面(奇数，偶数内部的相对顺序改变)
+void moveSingleBeforeDouble(int data[],int n)
+{
+	int start=0;
+	int end=n-1;
+	while(start<end)
+	{
+		while(start<end)
+		{
+		//奇数	
+		if(data[end]%2!=0)	break;
+		end--;
+		}
+		while(start<end)
+		{
+		//偶数 
+		if(data[start]%2==0) break;	
+		start++;
+		}
+		swap(data,start,end);
+		print(data,15); 
+	}
+	
+} 
+//把所有奇数移到所有偶数前面(奇数，偶数内部的相对顺序不变)
+void staticMoveSingleBeforeDouble(int data[],int n) 
+{
+	
+}
+//快排算法 中轴元素随机取
+void quickSortRandomAxis(int data[],int low,int high)
+{
+	if(low>=high) return;
+	int index=low+(rand())%(high-low);
+	swap(data,index,low); 
+	int i=low;
+	int j=high;
+	int axis=data[i];
+	while(i<j)
+	{
+		while(i<j)
+		{
+			if(data[j]<axis) break;
+			j--;
+		}
+	    swap(data,i,j);
+		while(i<j)
+		{
+			if(data[i]>axis) break;
+			i++;
+		}
+		swap(data,i,j);
+	}
+	print(data,17);
+	quickSortRandomAxis(data,low,i-1);
+	quickSortRandomAxis(data,i+1,high);
+} 
+//找出数组第k小的元素
+void getKMinValue(int data[],int low,int high,int k)
+{
+if(low>high) return;	
+int i=low;
+int j=high;
+int key=data[low];
+while(i<j)
+{
+while(i<j)
+{
+if(key>data[j]) break;
+j--;	
+}
+swap(data,i,j);
+while(i<j)
+{
+if(key<data[i])	break;
+i++; 
+}
+swap(data,i,j);	
+}
+//此时i为 数组中第i+1个小的元素
+if(i+1==k) printf("%d",data[i]);
+else if(i+1<k)
+{
+getKMinValue(data,i+1,high,k);	
+}
+else
+{
+getKMinValue(data,low,i-1,k);	
+} 	
+} 
+/*已知有n个正整数构成的集合A={ak|0<=k<n} 将其划分为两个不相交的子集A1 A2 
+元素的个数分别 n1,n2, A1和A2中的元素之和分别为s1,s2 满足n1-n2最小且s1-s2最大 
+题意为  从A中选取 n/2个最大的元素放到一边 选取n/2个最小的放到一边  
+*/ 
+void segArray(int data[],int low,int high,int n)
+{
+    if(low>high) return ;
+    int i=low;
+    int j=high;
+    int key=data[i];
+    while(i<j)
+    {
+    	while(i<j)
+    	{
+    		if(data[j]<key) break;
+    		j--;
+		}
+		swap(data,i,j);
+		while(i<j)
+		{
+			if(data[i]>key) break;
+			i++;
+		}
+		swap(data,i,j);
+	}
+	if(i-1==n/2) return; 
+	else if(i-1<n/2)segArray(data,i+1,high,n);	
+	else segArray(data,low,i-1,n);
+}
+//判断数组是否为极小堆
+bool isHeap(int data[],int i,int n) 
+{
+	int low=i*2+1;
+	int high=i*2+2;
+	if(low>=n) return true;
+	if(high>=n) 
+	{
+		if(data[low]>data[i]) return true;
+		else return false;
+	}
+	else
+	{
+		int j=low;
+		if(data[low]>data[high]) j=high;
+		if(data[j]<data[i]) return false;
+		return isHeap(data,low,n)&&isHeap(data,high,n);
+	}
+} 
 int main()
 {
-	int data[15]={13,54,32,12,1,4,14,18,23,16,45,99,15,17,16};
+	int data[15]={17,2,32,5,1,4,14,3,23,16,45,99,15,17,16};
 	int data2[17]={13,54,32,12,14,4,14,18,23,16,45,99,15,17,16,39,28};
 	int data3[5]={3,1,5,2,4};
 	//insertSort(data2,17);
@@ -373,5 +528,9 @@ int main()
 	//int tempt[15];
 	//mergeSort(0,14,data,tempt);
 	//radixSort(data,15,2);
-	quickSort2(data2,17);
+	//heapSort(data,15);
+	builHeap(data,15);
+	cout<<isHeap(data2,0,17)<<endl;
+	print(data,15);
+	cout<< isHeap(data,0,15)<<endl;
 } 
